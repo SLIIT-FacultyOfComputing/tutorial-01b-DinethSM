042b0754c07bdda26dbf7da980c1fb8f5c455f9f
IT21050008 <87351040+DinethSM@users.noreply.github.com>
2021-07-13 04:12:27 +0000

    code

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  int sub1,sub2;
+  float avg;
+
+  printf("Enter marks of Subject o1 :");
+  scanf("%d", &sub1);
+
+  printf("Enter marks of Subject o2 :");
+  scanf("%d", &sub2);
   
+  avg = (sub1 + sub2)/2;
+  printf("average is %.2f",avg);
+
   return 0;
 }
 
